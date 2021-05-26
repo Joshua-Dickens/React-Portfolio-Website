36ea1bf1142565174da744f2115a2d19a6ecbc95
Joshua <Joshua@Joshua-Dickens.com>
2021-05-26 16:03:53 -0700

    Added Sphere and Planet function for testing.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from 'react'
 import { Canvas, useFrame } from 'react-three-fiber'
+import { Sphere } from 'three'
 
 function Box(props) {
   const mesh = useRef()
@@ -25,14 +26,39 @@ function Box(props) {
   )
 }
 
+function Planet(props) {
+  const mesh = useRef()
+  const [state, setState] = useState({ isHovered: false, isActive: false })
+
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime()
+    mesh.current.position.x = mesh.current.position.x + Math.sin(time * 2) / 100
+    mesh.current.rotation.y = mesh.current.rotation.x += 0.005
+  })
+
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={state.isHovered ? [1.1, 1.1, 1.1] : [1, 1, 1]}
+      onClick={(e) => setState({ ...state, isActive: !state.isActive })}
+      onPointerOver={(e) => setState({ ...state, isHovered: true })}
+      onPointerOut={(e) => setState({ ...state, isHovered: false })}>
+      <sphereBufferGeometry args={[0.5, 40, 40]} />
+      <meshStandardMaterial color={state.isActive ? '#820263' : '#00FF00'} />
+    </mesh>
+  )
+}
+
 export default function App() {
   return (
     <Canvas>
-      <ambientLight intensity={0.5} />
+      <ambientLight intensity={0.1} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
       <pointLight position={[-10, -10, -10]} />
       <Box position={[-2, 0, 0]} />
       <Box position={[2, 0, 0]} />
+      <Planet position={[0, 0, 0]} />
     </Canvas>
   )
 }
