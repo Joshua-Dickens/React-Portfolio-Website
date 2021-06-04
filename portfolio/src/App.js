2398c7d319211c7176d791d35b4ca4082bafabb4
Joshua <Joshua@Joshua-Dickens.com>
2021-06-04 15:45:29 -0700

    Fixed rendering issue

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,60 @@
-import React, { useRef, useState } from 'react'
-import { Canvas, useFrame, useLoader } from 'react-three-fiber'
-import { TextureLoader } from 'three/src/loaders/TextureLoader'
-import { Sphere } from 'three'
+import React, { useRef, useState, Suspense } from 'react'
+import { Canvas, useFrame} from '@react-three/fiber'
+import { useTexture } from "@react-three/drei";
 
-function Box(props) {
+function Sun(props) {
+  const [colorMap] = useTexture(['Sun_texture.jpg'])
+  const mesh = useRef()
+  const [state, setState] = useState({ isHovered: false, isActive: false })
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime()
+    mesh.current.rotation.z = 0
+    mesh.current.rotation.x = 0
+    mesh.current.rotation.y = time / 5
+  })
+
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      
+      scale={[1, 1, 1]}
+      onPointerOver={(e) => setState({ ...state, isHovered: true })}
+      onPointerOut={(e) => setState({ ...state, isHovered: false })}>
+      <sphereGeometry args={[0.5, 100, 100]} map={colorMap} />
+      <meshStandardMaterial map={colorMap} transparent={true} emissive={'#444444'} emissiveIntensity={0.3}/>
+    </mesh>
+  )
+}
+
+function Earth(props) {
+  const mesh = useRef()
+  const [state, setState] = useState({ isHovered: false, isActive: false })
+  const [colorMap] = useTexture(['Earth_texture.jpg'])
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime()
+    mesh.current.position.x = 4 * Math.sin(time / 10)
+    mesh.current.position.y = 0
+    mesh.current.position.z = 4 * Math.cos(time / 10)
+    mesh.current.rotation.z = 0
+    mesh.current.rotation.x = 0
+    mesh.current.rotation.y = time / 2
+  })
+
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={[0.5, 0.5, 0.5]}
+      onPointerOver={(e) => setState({ ...state, isHovered: true })}
+      onPointerOut={(e) => setState({ ...state, isHovered: false })}>
+      <sphereBufferGeometry args={[0.5, 40, 40]} map={colorMap} />
+      <meshStandardMaterial map={colorMap} />
+    </mesh>
+  )
+}
+
+function Loading(props) {
   const mesh = useRef()
   const [state, setState] = useState({ isHovered: false, isActive: false })
 
@@ -27,68 +78,22 @@ function Box(props) {
   )
 }
 
-function Sun(props) {
-  const [colorMap] = useLoader(TextureLoader, ['Sun_texture.jpg'])
-  const mesh = useRef()
-  const [state, setState] = useState({ isHovered: false, isActive: false })
-
-  useFrame((state) => {
-    const time = state.clock.getElapsedTime()
-    mesh.current.rotation.z = 0
-    mesh.current.rotation.x = 0
-    mesh.current.rotation.y = time / 5
-  })
-
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      
-      scale={state.isHovered ? [1.1, 1.1, 1.1] : [1, 1, 1]}
-      onPointerOver={(e) => setState({ ...state, isHovered: true })}
-      onPointerOut={(e) => setState({ ...state, isHovered: false })}>
-      <sphereGeometry args={[0.5, 100, 100]} map={colorMap} />
-      <meshStandardMaterial map={colorMap}/>
-    </mesh>
-  )
-}
-
-function Earth(props) {
-  const mesh = useRef()
-  const [state, setState] = useState({ isHovered: false, isActive: false })
-  const [colorMap] = useLoader(TextureLoader, ['Earth_texture.jpg'])
-  useFrame((state) => {
-    const time = state.clock.getElapsedTime()
-    mesh.current.position.x = 3 * Math.sin(time / 10)
-    mesh.current.position.y = 0
-    mesh.current.position.z = 3 * Math.cos(time / 10)
-    mesh.current.rotation.z = 0
-    mesh.current.rotation.x = 0
-    mesh.current.rotation.y = time / 2
-  })
-
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={[0.5, 0.5, 0.5]}
-      onPointerOver={(e) => setState({ ...state, isHovered: true })}
-      onPointerOut={(e) => setState({ ...state, isHovered: false })}>
-      <sphereBufferGeometry args={[0.5, 40, 40]} map={colorMap} />
-      <meshStandardMaterial map={colorMap} />
-    </mesh>
-  )
-}
-
-
-
 export default function App() {
   return (
-    <Canvas camera={{position: [0, 2, 8], rotation: [0, -45, 0]}}>
-      <ambientLight intensity={0.1} />
-      <pointLight position={[0, 0, 0]} />
+    <Canvas camera={{position: [0, 2, 8]}}>
+      <Suspense fallback={<Loading />}>
       <Sun position={[0, 0, 0]} />
       <Earth position={[3, 0, 0]} />
+      </Suspense>
+      <ambientLight intensity={0.02}/>
+      <pointLight position={[0, 0, 0]}/>
+      
+      <spotLight position={[1.5, 0, 0]} distance={4}/>
+      <spotLight position={[-1.5, 0, 0]} distance={4}/>
+      <spotLight position={[0, 0, -1.5]} distance={4}/>
+      <spotLight position={[0, 0, 1.5]} distance={4}/>
+      <spotLight position={[0, 1.5, 0]} distance={4}/>
+      <spotLight position={[0, -1.5, 0]} distance={4}/>
     </Canvas>
   )
 }
